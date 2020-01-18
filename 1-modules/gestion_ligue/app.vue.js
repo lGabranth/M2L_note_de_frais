19ea22a4ebdf3e34d3991a7cddf3c4b2cfc6fe70
Charles Bourtoire <socrazyaboutsky@gmail.com>
2020-01-18 16:16:29 +0100

    module ligue prêt

--- 1-modules/gestion_ligue/app.vue.js
+++ 1-modules/gestion_ligue/app.vue.js
@@ -8,6 +8,8 @@ var vue = new Vue({
 			nom:'',
 			id_utilisateur:0,
 		},
+		modif:{},
+		suppr:{},
 	},
 	mounted(){
 		this.GetListeLigue();
@@ -48,6 +50,8 @@ var vue = new Vue({
 		},
 
 		OuvrirModalAjout:function(){
+			this.ajout.nom = '';
+			this.ajout.id_utilisateur = 0;
 			$('#modal_ajout').modal('show');
 		},
 
@@ -60,6 +64,87 @@ var vue = new Vue({
 				data:{},
 				success:function(res){
 					scope.liste_utilisateurs = JSON.parse(res);
+				},
+				error:function(){
+				}
+			});
+		},
+
+		AjoutLigue:function(){
+			var scope = this;
+
+			if(scope.ajout.nom == ''){
+				Notify('info','Veuillez saisir un nom');
+				return;
+			}
+
+			$.ajax({
+				url:"data.php?cas=ajout_ligue",
+				type:"POST",
+				data:scope.ajout,
+				success:function(res){
+					if(res == -2) Notify('info','Veuillez saisir un nom');
+					if(res == -1) Notify('warning','Ce nom est déjà attribué');
+					if(res == 1){
+						Notify('success',`La ligue ${scope.ajout.nom} a été crée`);
+						// Notify('success','La ligue '+scope.ajout.nom+' a été crée');
+						scope.ajout.nom = '';
+						scope.ajout.id_utilisateur = 0;
+						scope.GetListeLigue();
+						$('#modal_ajout').modal('hide');
+						scope.GetListeUtilisateur();
+					}
+				},
+				error:function(){
+				}
+			});
+		},
+
+		OuvrirModalModif:function(elem){
+			this.modif = JSON.parse(JSON.stringify(elem));
+			$('#modal_modif').modal('show');
+		},
+
+		ModifierLigue:function(){
+			var scope = this;
+			if(scope.modif.nom == ''){
+				Notify('danger','Vous ne pouvez pas supprimer le nom d\'une ligue');
+				return;
+			}
+
+			$.ajax({
+				url:"data.php?cas=modif_ligue",
+				type:"POST",
+				data:scope.modif,
+				success:function(res){
+					Notify('success',`Ligue "${scope.modif.nom}" modifiée`);
+					scope.GetListeLigue();
+					$('#modal_modif').modal('hide');
+					scope.modif = {};
+				},
+				error:function(){
+				}
+			});
+		},
+
+		OuvrirModalSuppr:function(elem){
+			this.suppr = JSON.parse(JSON.stringify(elem));
+			$('#modal_suppr').modal('show');
+		},
+
+		SupprimerLigue:function(){
+			var scope = this;
+
+			$.ajax({
+				url:"data.php?cas=suppr_ligue",
+				type:"POST",
+				data:scope.suppr,
+				success:function(res){
+					Notify('success',`La ligue ${scope.suppr.nom} a été suprimée`);
+					scope.GetListeLigue();
+					$('#modal_suppr').modal('hide');
+					scope.GetListeUtilisateur();
+					scope.suppr = {};
 				},
 				error:function(){
 				}
